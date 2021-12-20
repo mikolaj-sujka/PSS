922e1aa44bef05928d4c09dc20ee184af89b3252
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2021-12-20 20:08:09 +0100

    lading page etc

--- pss_frontend/pss-front/src/app/app.module.ts
+++ pss_frontend/pss-front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsViewComponent } from './home-page/news-view/news-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    HomePageComponent,
+    FooterComponent,
+    NewsViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ pss_frontend/pss-front/src/app/home-page/news-view/news-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-view',
+  templateUrl: './news-view.component.html',
+  styleUrls: ['./news-view.component.scss']
+})
+export class NewsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
