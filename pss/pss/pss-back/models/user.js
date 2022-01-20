c1dfde75a2cf86c2787d526b4ee20bbbe49aed43
Lukasz <freshbenypl@gmail.com>
2022-01-20 22:35:26 +0100

    wyjebane z usera miasto i dyscyplina jak legia w ekstraklasie

--- pss/pss/pss-back/models/user.js
+++ pss/pss/pss-back/models/user.js
@@ -4,15 +4,13 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  name: {type: String},
-  city: {type: String},
-  discipline: {type: String},
-  team: {type: String},
-  age: {type: Number},
-  weight: {type: Number},
-  height: {type: Number},
-  img: {type: String},
-  role: {type: String}
+  name: { type: String },
+  team: { type: String },
+  age: { type: Number },
+  weight: { type: Number },
+  height: { type: Number },
+  img: { type: String },
+  role: { type: String },
 });
 
 userSchema.plugin(uniqueValidator);
--- pss/pss/src/app/services/user.service.ts
+++ pss/pss/src/app/services/user.service.ts
@@ -1,22 +1,20 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
-import {User} from "../models/user.model";
-import {NgForm} from "@angular/forms";
-import {environment} from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { NgForm } from '@angular/forms';
+import { environment } from '../../environments/environment';
 
-const apiUrl = environment.apiUrl + "/user/"
+const apiUrl = environment.apiUrl + '/user/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-  }
-
-  findUsers(name: string, city: string, discipline: string): Observable<User[]> {
-    return this.http.get<User[]>(apiUrl + "find/" + name + "-" + city + "-" + discipline);
+  findUsers(name: string): Observable<User[]> {
+    return this.http.get<User[]>(apiUrl + 'find/' + name);
   }
 
   getUserById(id: string): Observable<User> {
@@ -24,16 +22,12 @@ export class UserService {
   }
 
   updateUserData(id: string, form: NgForm, email: string): Observable<Object> {
-    return this.http.post(apiUrl + id,
-      {
-        "email": email,
-        "name": form.value.name,
-        "city": form.value.city,
-        "discipline": form.value.discipline,
-        "age": form.value.age,
-        "weight": form.value.weight,
-        "height": form.value.height
-      })
-
+    return this.http.post(apiUrl + id, {
+      email: email,
+      name: form.value.name,
+      age: form.value.age,
+      weight: form.value.weight,
+      height: form.value.height,
+    });
   }
 }
